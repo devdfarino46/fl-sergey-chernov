c3f29e97e88f041c9235f44fdb5890b683ec38a4
Dmitry Farino <dfarino46@gmail.com>
2024-11-03 01:05:42 +0300

    All fixed

--- js-src/main.js
+++ js-src/main.js
@@ -9,6 +9,8 @@ const aboutVideo = document.querySelector('.about-video');
 const reviewItems = document.querySelectorAll('.review');
 const reviews = document.querySelector('.reviews');
 const services = document.querySelector('.services');
+const about = document.querySelector('.about');
+const reviewPhotos = document.querySelectorAll('.review-photo');
 
 if (header && menu) {
   const menuBtn = header.querySelector('.header__menu-btn');
@@ -61,6 +63,10 @@ if (hero) {
   });
 }
 
+if (about) {
+  // const bg = 
+}
+
 if (faq) {
   itemFaqs.forEach((itemFaq, index) => {
     const btn = itemFaq.querySelector('.item-faq__button');
@@ -76,6 +82,31 @@ if (faq) {
     });
   });
 }
+
+reviewPhotos.forEach((reviewPhoto, index) => {
+  /**
+   * @type {HTMLVideoElement}
+   */
+  const video = reviewPhoto.querySelector('.review-photo>video');
+
+  reviewPhoto.addEventListener('mousemove', () => {
+    video.play();
+  });
+
+  reviewPhoto.addEventListener('mouseleave', () => {
+    video.currentTime = 0;
+    video.pause();
+  });
+  
+  document.addEventListener('touchmove', (ev) => {
+    if (ev.composedPath().includes(reviewPhoto)) {
+      video.play();
+    } else {
+      video.currentTime = 0;
+      video.pause();
+    }
+  })
+});
 
 if (aboutVideo) {
   const btn = aboutVideo.querySelector('.about-video>button');
@@ -108,7 +139,21 @@ if (reviews) {
       nextEl: '.reviews__slider .swiper-button-next',
       prevEl: '.reviews__slider .swiper-button-prev',
     }
-  })
+  });
+  const slideRel = reviews.querySelector('.swiper-slide.rel');
+  const fiers = slideRel.querySelector('.fiers');
+  const slideRelLeft = slideRel.getBoundingClientRect().left;
+  let slideRelTranslate = 0;
+  const moveFiers = () => {
+    const slideRelMove = slideRel.getBoundingClientRect().left - slideRelLeft;
+    slideRelTranslate = slideRelMove * 0.1;
+
+    if (Math.abs(slideRelTranslate) <= 70) {
+      fiers.style.transform = `translateX(${slideRelTranslate}px)`;
+    }
+  }
+  slider.on('sliderMove', () => {moveFiers(); fiers.style.transition = 'none'});
+  slider.on('setTransition', () => {moveFiers(); fiers.style.transition = '.5s linear'});
 }
 
 reviewItems.forEach(review => {
@@ -146,4 +191,14 @@ if (services) {
       }
     })
   });
+
+  services.addEventListener('mousemove', ev => {
+    if (window.innerWidth <= 640) {
+      items.forEach(item => {
+        const icon = item.querySelector('._icon');
+        icon.style.left = ev.clientX+ 'px';
+        icon.style.top = ev.clientY+ 'px';
+      })
+    }
+  })
 }
